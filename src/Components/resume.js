88954ef84b04d73eedc846884cb1d3b959ca74a7
Emil <emil.capito@autoipacket.com>
2019-06-06 13:28:12 -0400

    poop

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 import Education from "./education";
+import Education2 from "./education2";
 import Experience from "./experience";
 import Skills from "./skills";
 
@@ -28,11 +29,11 @@ class Resume extends Component {
             </p>
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             <h5>Address</h5>
-            <p>1 Hacker Way Menlo Park, 94025</p>
+            <p>39 Floral Drive Wheeling, WV</p>
             <h5>Phone</h5>
-            <p>(123) 456-7890</p>
+            <p>(304) 280-4470</p>
             <h5>Email</h5>
-            <p>someone@example.com</p>
+            <p>emil.capito@autoipacket.com</p>
             <h5>Web</h5>
             <p>mywebsite.com</p>
             <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
@@ -42,21 +43,21 @@ class Resume extends Component {
             <Education
               startYear={2017}
               endYear={2021}
-              schoolName="My University"
-              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              schoolName="Benjamin M. Statler College of Engineering and Mineral Resources
+              West Virginia University Honors College"
+              schoolDescription="Bachelor of Science in Computer Engineering: Degree to be conferred May 2021"
             />
-            <Education
-              startYear={2017}
-              endYear={2021}
-              schoolName="My Second University"
-              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            <Education2
+              info="March 2019"
+              schoolName="Education Abroad"
+              schoolDescription="Munich, Germany: Short-term faculty led engineering study abroad"
             />
             <hr style={{ borderTop: "3px solid #e22947" }} />
             <h2>Experience</h2>
             <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
+              startYear="Spring 2018"
+              endYear="Summer 2018"
+              jobName="Deli/Cheese Associate"
               jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
             />
             <Experience
